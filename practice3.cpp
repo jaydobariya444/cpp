79af2a30db2ce15fd3e78420b913a9da99cfd07e
jaydobariya444 <sq.jay444@gmail.com>
2024-06-14 11:06:48 +0530

    jay

---
+++ practice3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    
+
+    for(int i=0; i<10; i++){
+        cout<<int(i);
+    }
+
+    cout<<endl;    
+
+    for(int i=10; i>0; i--){
+        cout<<int(i);
+    }
+
+
+
+}
