3ea6b1d458e959e2f86a4ab583a565f8f2e22751
jaydobariya444 <sq.jay444@gmail.com>
2024-06-14 10:47:11 +0530

    12

---
+++ practice1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cout<<"enter number of a:-";
+    cin>>a;
+
+    cout<<"enter number of b:-";
+    cin>>b;
+
+    if(a<b){
+        cout<<"b is larger number";
+
+    }else{
+        cout<<"a is larger number";
+
+    }
+
+    return 0;
+
+}
---
+++ practice2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int mt,py,ch,total;
+    float per;
+
+    cout<<"enter mark of mt:-";
+    cin>>a;
+}
