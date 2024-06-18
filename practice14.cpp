4e60050ad37e04ee8ef10ea87ad29496eb1f6c1d
jaydobariya444 <sq.jay444@gmail.com>
2024-06-18 10:04:13 +0530

    123

---
+++ practice14.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+    using namespace std;
+
+    int main(){
+
+        float basicsalary,hra,da,gross;
+
+        cout<<"enter salary:-";
+        cin>>basicsalary;
+
+
+    return 0;
+}
